6cbd539804d6be23d895baa7b7d2b9be4c444c65
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-17 17:32:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
-  res.render("index")
-});
 
 
 app.use('/products', productsRoutes);
