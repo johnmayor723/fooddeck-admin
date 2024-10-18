0b1df30afda384097b8e3924cdaa97a2ea369bc3
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-18 11:05:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,18 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-
-const indexRoute = require('./routes/IndexRoute')
+const indexRoute = require('./routes/IndexRoute');
 const productsRoutes = require('./routes/products');
 const ordersRoutes = require('./routes/orders');
 
 const app = express();
 
-
-
 app.set('view engine', 'ejs');
 
-
-/*
-
+// Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, '../uploads/');
+    const uploadPath = path.join(__dirname, 'uploads');
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath);
     }
@@ -31,39 +26,18 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + '-' + file.originalname);
   },
 });
+const upload = multer({ storage: storage });
 
-const upload = multer({ storage });
-
-*/
-
-
+// Middleware and Static Configurations
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, '../uploads/');
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath);
-    }
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
+// Routes setup
 app.use('/', indexRoute);
-app.use('/products', productsRoutes);
+app.use('/products', productsRoutes(upload));  // Passing 'upload' middleware to product routes
 app.use('/orders', ordersRoutes);
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
