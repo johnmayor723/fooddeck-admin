2354047b37f21b09eb35d786a433ffb7fcbd964d
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-12-11 05:37:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/', indexRoute);
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3060;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
